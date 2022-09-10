d3bed06174d50bb6c37c6c9ad8de8d1dcefc262e
Adam Heins <mail@adamheins.com>
2022-09-10 15:10:04 -0400

    Clean up Lab 2 starter code.

--- lab2/nodes/lab2.py
+++ lab2/nodes/lab2.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python
 import rospy
 import math
-import time
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
 
-def publisher_node():
-    '''
-    TODO: complete the publisher function here
-    '''
-    pass
-
-
 def main():
+    rospy.init_node("lab02")
 
-    try:
-        rospy.init_node('lab02')
-        publisher_node()
-    except rospy.ROSInterruptException:
-        pass
+    """TODO: publish commands to make robot move to desired pose"""
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
