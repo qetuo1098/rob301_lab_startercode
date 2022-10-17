5df688f814404096f2e808253c038feb3c9f468e
Adam Heins <mail@adamheins.com>
2022-10-17 17:48:46 -0400

    Improvements to Lab 4 starter code.

--- lab4/nodes/noisy_vel_publisher.py
+++ lab4/nodes/noisy_vel_publisher.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 """Add noise to cmd_vel for use in simulation."""
 import rospy
-import time
-from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
@@ -18,19 +16,17 @@ class NoiseMaker(object):
         self.cmd_sub = rospy.Subscriber("cmd_vel", Twist, self.cmd_callback)
         self.cmd_pub = rospy.Publisher("cmd_vel_noisy", Twist, queue_size=1)
 
-    def cmd_callback(self, cmd_msg):
+    def cmd_callback(self, msg):
         # noisy twist is the same but with Gaussian noise on linear x
-        twist = Twist()
-        twist.linear.x = cmd_msg.linear.x + np.random.normal(
-            loc=self.mu, scale=self.std
-        )
+        twist = msg
+        twist.linear.x += np.random.normal(loc=self.mu, scale=self.std)
         self.cmd_pub.publish(twist)
 
 
 if __name__ == "__main__":
     rospy.init_node("noisyvel")
 
-    mu_noise = -0.001
-    std_noise = 0.001
+    mu_noise = 0.0  # mean
+    std_noise = 0.01  # standard deviation
     noise_maker = NoiseMaker(mu=mu_noise, std=std_noise)
     rospy.spin()
--- lab4/nodes/scan_angle_publisher.py
+++ lab4/nodes/scan_angle_publisher.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 import numpy as np
 
@@ -13,16 +12,26 @@ class ScanAnglePublisher(object):
 
     def scan_cb(self, msg):
         # only use the scan where y >= 0 (i.e., to the left side of the robot)
-        in_range = np.array(scan.ranges[0:180])
-        angles = np.arange(0, 180) * np.pi / 180
+        in_range = np.array(msg.ranges[0:180])
+        angles = np.deg2rad(np.arange(0, 180))
 
         # computes the y-distance to each measurement
         y_range = in_range * np.sin(angles)
 
-        # filter out measurements with ranges larger than 3m or y_range outside of 0.5-0.65m
-        angles = angles[(y_range <= 0.65) & (y_range >= 0.5) & (in_range < 3)]
+        # filter out measurements with ranges outside of [0.1, 3]m or y_range
+        # outside of [0.5, 0.65]m
+        # note that the range is 0 when no valid range is obtained: this is
+        # filtered out by the 0.1m lower bound
+        angles = angles[
+            (0.5 <= y_range) & (y_range <= 0.65) & (0.1 <= in_range) & (in_range <= 3)
+        ]
 
-        # the median is NaN if there are no valid angles
+        # if no valid measurements, don't publish anything
+        if len(angles) == 0:
+            return
+
+        # if we have multiple measurements (i.e. multiple lasers hit one or
+        # more objects), take the median
         angle = np.median(angles)
 
         # publish the message
@@ -35,7 +44,6 @@ def main():
     rospy.init_node("scan_angle_publisher")
     pub = ScanAnglePublisher()
     rospy.spin()
-
 
 
 if __name__ == "__main__":
